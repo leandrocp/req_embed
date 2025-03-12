feb1ffbbb6c60614a13809b02681870d6a5bdec9
Leandro Pereira <leandro@leandro.io>
2025-03-12 14:53:30 -0400

    optimize get_by_url

--- lib/req_embed/providers.ex
+++ lib/req_embed/providers.ex
@@ -3,6 +3,29 @@ defmodule ReqEmbed.Providers do
 
   @external_resource "priv/vendor/providers.json"
 
+  @popular_providers [
+    "YouTube",
+    "Facebook",
+    "Instagram",
+    "Twitter",
+    "TikTok",
+    "LinkedIn",
+    "Pinterest",
+    "Vimeo",
+    "Spotify",
+    "SoundCloud",
+    "Dailymotion",
+    "Giphy",
+    "Twitch",
+    "Reddit",
+    "Flickr",
+    "CodePen",
+    "Figma",
+    "Canva",
+    "Loom",
+    "Miro"
+  ]
+
   @providers Path.join([Application.app_dir(:req_embed), "priv/vendor/providers.json"])
              |> File.read!()
              |> Jason.decode!()
@@ -30,10 +53,20 @@ defmodule ReqEmbed.Providers do
                }
              end)
 
+  @popular_provider_list Enum.filter(@providers, &(&1.name in @popular_providers))
+  @other_provider_list Enum.filter(@providers, &(&1.name not in @popular_providers))
+
   def all, do: @providers
 
   def get_by_url(url) when is_binary(url) do
-    Enum.find(@providers, fn provider ->
+    case find_matching_provider(@popular_provider_list, url) do
+      nil -> find_matching_provider(@other_provider_list, url)
+      provider -> provider
+    end
+  end
+
+  defp find_matching_provider(providers, url) do
+    Enum.find(providers, fn provider ->
       Enum.any?(provider.endpoints, fn endpoint ->
         Enum.any?(endpoint.schemes, fn pattern ->
           Regex.match?(pattern, url)
