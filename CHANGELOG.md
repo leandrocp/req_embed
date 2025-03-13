2951ac68686f568f501bbb11d173896f3ea84460
Leandro Pereira <leandro@leandro.io>
2025-03-13 11:22:25 -0400

    v0.1.2

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
 # Changelog
 
+## 0.1.2 (2025-03-13)
+
+### Changed
+  * Removed `ReqEmbed.Content` in favor of `ReqEmbed.Link`
+
 ## 0.1.1 (2025-03-12)
 
 ### Added
--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule ReqEmbed.MixProject do
   use Mix.Project
 
   @source_url "https://github.com/BeaconCMS/req_embed"
-  @version "0.1.1"
+  @version "0.1.2"
 
   def project do
     [
